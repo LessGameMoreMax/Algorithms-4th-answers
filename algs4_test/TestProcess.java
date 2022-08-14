871c1d60b7130474485458882c2996d61a150680
LessGameMoreMax <LessGameMoreMax@outlook.com>
2022-08-14 09:19:00 +0800

    Add Algorithms 4th

---
+++ algs4_test/TestProcess.java
@@ -0,0 +1,15 @@
+package algs4_test;
+import edu.princeton.cs.algs4.StdDraw;
+
+public class TestProcess {
+	public static void main(String[] args) {
+		String s = "ACjahgds";
+		System.out.println(s.substring(3, 5));
+	}
+}
+
+		
+
+
+
+
